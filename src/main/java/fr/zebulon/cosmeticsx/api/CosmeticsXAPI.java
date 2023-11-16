1a9306b69aa9509a51fc6510d210bfa7d844a722
Estéban Piecourt <ryzelachaineofficiel@gmail.com>
2023-11-16 16:56:25 +0100

    (16/11) - v1.0.0 Dev

---
+++ src/main/java/fr/zebulon/cosmeticsx/api/CosmeticsXAPI.java
@@ -0,0 +1,19 @@
+package fr.zebulon.cosmeticsx.api;
+
+import fr.zebulon.cosmeticsx.CosmeticsX;
+import fr.zebulon.cosmeticsx.cosmetics.AbstractCosmetic;
+import org.bukkit.entity.Player;
+
+public class CosmeticsXAPI {
+
+    private final CosmeticsX plugin;
+
+    public CosmeticsXAPI(CosmeticsX plugin) {
+        this.plugin = plugin;
+    }
+
+    // TODO : Implement this method
+    public void equip(Player player, AbstractCosmetic cosmetic) {
+
+    }
+}
