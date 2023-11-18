b58fc063c52848f96f3af966c9f54d03ab52923e
Estéban Piecourt <42072211+estxbxn@users.noreply.github.com>
2023-11-18 23:48:02 +0100

    (18/11) - Update ISlotHandler

--- src/main/java/fr/zebulon/cosmeticsx/cosmetics/factory/properties/ISlotHandler.java
+++ src/main/java/fr/zebulon/cosmeticsx/cosmetics/factory/properties/ISlotHandler.java
@@ -1,2 +1,21 @@
-package fr.zebulon.cosmeticsx.cosmetics.factory.properties;public interface ISlotHandler {
+package fr.zebulon.cosmeticsx.cosmetics.factory.properties;
+
+import fr.zebulon.cosmeticsx.cosmetics.CosmeticSlot;
+
+import java.util.Optional;
+
+public interface ISlotHandler {
+	/**
+	 * Get the type of the slot where the cosmetic is
+	 *
+	 * @return the slot
+	 */
+	Optional<CosmeticSlot> getSlot();
+
+	/**
+	 * Set the cosmetic on a cosmetic slot using {@link CosmeticSlot}
+	 *
+	 * @param slot the slot
+	 */
+	void setSlot(CosmeticSlot slot);
 }
